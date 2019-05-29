60702e7f35111b562731b8179d4d22044d39830f
liqiangbiao <liqiangbiao@ciyun.cn>
2019-05-29 10:47:22 +0800

    修改

--- util/src/main/java/com/sangbill/design/template/ActStateUtil.java
+++ util/src/main/java/com/sangbill/design/template/ActStateUtil.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import com.sangbill.design.template.enumdef.ActForm.EActForm;
 import com.sangbill.design.template.handler.BeanActStateHandler;
+import com.sangbill.design.template.handler.CheckinActStateHandler;
+import com.sangbill.design.template.handler.KnowledgeActStateHandler;
 import com.sangbill.design.template.handler.TeamActStateHandler;
 import com.sangbill.util.SpringUtils;
 
@@ -15,6 +17,10 @@ public class ActStateUtil {
 			handler = SpringUtils.getBean(TeamActStateHandler.class);
 		}else if(form == EActForm.ACT_BEAN.key){
 			handler = SpringUtils.getBean(BeanActStateHandler.class);
+		}else if(form == EActForm.ACT_CHECKIN.key){
+			handler = SpringUtils.getBean(CheckinActStateHandler.class);
+		}else if(form == EActForm.ACT_KNOWLEDGE.key){
+			handler = SpringUtils.getBean(KnowledgeActStateHandler.class);
 		}
 		return handler.getActState(state, startTime, endTime, enterStartTime, enterEndTime, actDetailJson);
 	}
