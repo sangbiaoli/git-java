93f570c1a4afd141a7e50cde52b6cf0f1c248ad5
734156776@qq.com <734156776@qq.com>
2020-04-07 22:05:11 +0800

    参照官网

--- util/src/main/java/com/sangbill/leecode/tree/T0106_buildTree.java
+++ util/src/main/java/com/sangbill/leecode/tree/T0106_buildTree.java
@@ -1,33 +1,45 @@
 package com.sangbill.leecode.tree;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class T0106_buildTree {
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(inorder == null || inorder.length == 0)
-            return null;
+	int postIdx;
+	int[] postorder;
+	int[] inorder;
+	HashMap<Integer, Integer> idxMap = new HashMap<Integer, Integer>();
 
-        int len = inorder.length;
-        if(len == 1)
-            return new TreeNode(inorder[0]);
-        
-        int val = postorder[len - 1];
-        int i = 0;
-        for(;i < len;i++){
-            if(inorder[i] == val)
-                break;
-        }
+	public TreeNode helper(int inLeft, int inRight) {
+		// if there is no elements to construct subtrees
+		if (inLeft > inRight)
+			return null;
 
-        TreeNode root = new TreeNode(val);
+		// pick up post_idx element as a root
+		int root_val = postorder[postIdx];
+		TreeNode root = new TreeNode(root_val);
 
-        root.left = buildTree(
-            Arrays.copyOfRange(inorder,0,i),
-            Arrays.copyOfRange(postorder,0,i)
-        );
-        root.right = buildTree(
-            Arrays.copyOfRange(inorder,i+1,len),
-            Arrays.copyOfRange(postorder,i,len-1)
-        );
-        return root;
-    }
+		// root splits inorder list
+		// into left and right subtrees
+		int index = idxMap.get(root_val);
+
+		// recursion
+		postIdx--;
+		// build right subtree
+		root.right = helper(index + 1, inRight);
+		// build left subtree
+		root.left = helper(inLeft, index - 1);
+		return root;
+	}
+
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		this.postorder = postorder;
+		this.inorder = inorder;
+		// start from the last postorder element
+		postIdx = postorder.length - 1;
+
+		// build a hashmap value -> its index
+		int idx = 0;
+		for (Integer val : inorder)
+			idxMap.put(val, idx++);
+		return helper(0, inorder.length - 1);
+	}
 }
