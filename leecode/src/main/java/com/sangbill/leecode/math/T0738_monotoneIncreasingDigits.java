826aac304584d257531afe39a54bee247a32a6fd
734156776@qq.com <734156776@qq.com>
2020-12-16 23:07:33 +0800

    加法

--- leecode/src/main/java/com/sangbill/leecode/math/T0738_monotoneIncreasingDigits.java
+++ leecode/src/main/java/com/sangbill/leecode/math/T0738_monotoneIncreasingDigits.java
@@ -2,37 +2,17 @@ package com.sangbill.leecode.math;
 
 class T0738_monotoneIncreasingDigits {
     public int monotoneIncreasingDigits(int N) {
-        if (N <= 9) {
-            return N;
-        }
-        int[] nums = new int[10];
-        int count = 0;
-        while (N > 0) {
-            nums[count] = N % 10;
-            N = N / 10;
-            count++;
-        }
-
-        for (int i = count; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
-                int j = i;
-                while (j < count && nums[j] == nums[j + 1]) {
-                    j++;
-                }
-                nums[j]--;
-                j--;
-                while (j >= 0) {
-                    nums[j] = 9;
-                    j--;
-                }
+        int ones = 111111111;
+        int res = 0;
+        for (int i = 0; i < 9; i++) {
+            while (res + ones > N) {
+                ones /= 10;
             }
+            res += ones;
+            if (ones == 0)
+                break;
         }
-
-        int rs = 0;
-        for (int i = count; i >= 0; i--) {
-            rs = rs * 10 + nums[i];
-        }
-        return rs;
+        return res;
     }
 
     public static void main(String[] args) {
