77d76e13a2cfae66290b33d1ce32cd5956659553
734156776@qq.com <734156776@qq.com>
2020-12-19 13:03:03 +0800

    面试题 16.11. 跳水板

---
+++ leecode/src/main/java/com/sangbill/leecode/view/T1611_divingBoard.java
@@ -0,0 +1,17 @@
+package com.sangbill.leecode.view;
+
+class T1611_divingBoard {
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (k == 0) {
+            return new int[0];
+        }
+        if (shorter == longer) {
+            return new int[]{shorter * k};
+        }
+        int[] lengths = new int[k + 1];
+        for (int i = 0; i <= k; i++) {
+            lengths[i] = shorter * (k - i) + longer * i;
+        }
+        return lengths;
+    }
+}
