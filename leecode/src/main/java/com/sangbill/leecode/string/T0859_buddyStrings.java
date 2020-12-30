c3eab4f6d99ce7feb068aa2204e74f66db138846
734156776@qq.com <734156776@qq.com>
2020-12-31 00:34:07 +0800

    859. 亲密字符串

---
+++ leecode/src/main/java/com/sangbill/leecode/string/T0859_buddyStrings.java
@@ -0,0 +1,45 @@
+package com.sangbill.leecode.string;
+
+class T0859_buddyStrings {
+    public boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) {
+            return false;
+        }
+        int[] nums = new int[26];
+        int start = -1, end = -1, cnt = 0;
+        char[] a = A.toCharArray();
+        char[] b = B.toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            nums[a[i] - 'a']++;
+            if (a[i] != b[i]) {
+                cnt++;
+                if (start == -1) {
+                    start = i;
+                } else {
+                    end = i;
+                }
+                if (cnt >= 3) {
+                    return false;
+                }
+            }
+        }
+        //不同位置数量只有三种情况：0个，1个，2个
+        if (cnt == 0) {
+            for (int i : nums) {
+                if (i >= 2) {
+                    return true;
+                }
+            }
+            return false;
+        } else if (cnt == 1) {
+            return false;
+        } else {
+            return (a[start] == b[end] && a[end] == b[start]);
+        }
+    }
+
+    public static void main(String[] args) {
+        T0859_buddyStrings su = new T0859_buddyStrings();
+        System.out.println(su.buddyStrings("abab", "abab"));
+    }
+}
