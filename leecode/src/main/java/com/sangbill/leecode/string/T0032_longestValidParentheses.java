6ab92dc18210622030ecac576b3b5e92ff1f89fa
liqiangbiao <liqiangbiao@ciyun.cn>
2021-01-01 11:23:39 +0800

    优化版

--- leecode/src/main/java/com/sangbill/leecode/string/T0032_longestValidParentheses.java
+++ leecode/src/main/java/com/sangbill/leecode/string/T0032_longestValidParentheses.java
@@ -1,5 +1,4 @@
 package com.sangbill.leecode.string;
-import java.util.Stack;
 
 class T0032_longestValidParentheses {
     public int longestValidParentheses(String s) {
@@ -7,43 +6,36 @@ class T0032_longestValidParentheses {
             return 0;
         }
         char[] c = s.toCharArray();
-        int[] left = new int[c.length / 2];
-        int[] right = new int[c.length / 2];
-        Stack<Integer> stack = new Stack<>();
-        int index = -1, max = 0;
+        int left = 0, right = 0, max = 0;
         for (int i = 0; i < c.length; i++) {
             if (c[i] == '(') {
-                stack.push(i);
-                continue;
+                left++;
+            } else {
+                right++;
             }
-            if (stack.size() > 0) {
-                if (index == -1) { //第一次
-                    ++index;
-                    left[index] = stack.pop();
-                    right[index] = i;
-                } else {
-                    int temp = stack.pop();
-                    if (temp + 1 == left[index]) { //匹配这个模式(())，合并
-                        left[index] = temp;
-                        right[index] = i;
-                    } else if (right[index] + 1 == temp) { //匹配这个模式()()，合并
-                        right[index] = i;
-                    } else {   //不匹配这两种，则新加一堆
-                        ++index;
-                        left[index] = temp;
-                        right[index] = i;
-                    }
-                }
-                //做()()的合并
-                while (index > 0) {
-                    if (right[index - 1] + 1 == left[index]) {
-                        right[index - 1] = right[index];
-                        --index;
-                    } else {
-                        break;
-                    }
-                }
-                max = Math.max(max, right[index] - left[index] + 1);
+            if (left == right) {
+                max = Math.max(max, left * 2);
+            }
+            if (left < right) {
+                left = 0;
+                right = 0;
+            }
+        }
+
+        left = 0;
+        right = 0;
+        for (int i = c.length - 1; i >= 0; i--) {
+            if (c[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                max = Math.max(max, left * 2);
+            }
+            if (left > right) {
+                left = 0;
+                right = 0;
             }
         }
         return max;
