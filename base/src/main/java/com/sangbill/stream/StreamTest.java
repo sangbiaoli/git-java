a9a05afcf914ad644aabfd704ddb871f6a4a9928
liqiangbiao <liqiangbiao@ciyun.cn>
2021-04-01 11:02:51 +0800

    添加流

---
+++ base/src/main/java/com/sangbill/stream/StreamTest.java
@@ -0,0 +1,13 @@
+package com.sangbill.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest {
+	public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.stream();
+	}
+}
